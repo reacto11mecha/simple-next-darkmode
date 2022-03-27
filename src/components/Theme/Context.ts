8d2e9ddecbde52b44eb277da95aefdeb4c874fab
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-03-27 16:00:07 +0700

    fix: eslint warning

--- src/components/Theme/Context.ts
+++ src/components/Theme/Context.ts
@@ -1,4 +1,5 @@
 import React from "react";
 import type { contextInterface } from "./types";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 export const DarkModeContext = React.createContext<contextInterface>(null!);
